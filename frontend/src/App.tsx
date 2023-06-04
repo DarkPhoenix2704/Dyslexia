b90e15c764836094a52eb6d4b5f03d82257c71f3
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-06-04 21:37:20 +0530

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col h-[100vh] items-center justify-center bg-[#1e1e20] text-white">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
